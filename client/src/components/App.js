a8e4b6aad54f1b58b163752a7a4896c1771aae50
M Alexander Moroney <alexmoroney@gmail.com>
2022-11-19 12:40:47 -0500

    Fix the bug with history Needed to import history.js into the App.js component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { Router, Link, Route } from "react-router-dom";
 import ReportCreate from "./Reports/ReportCreate";
 import ReportList from "./Reports/ReportList";
 import ReportEdit from "./Reports/ReportEdit";
 import ReportDelete from "./Reports/ReportDelete";
 import ReportDownload from "./Reports/ReportDownload";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           {/* To ensure that the Header component is always visible we embed it wihin the BrowserRounter component without a specified path. This is ensure that is is present on each Route*/}
           <Header />
@@ -22,7 +23,7 @@ const App = () => {
           <Route path="/reports/download:" exact component={ReportDownload} />
           {/* I might add another path for ReportShow in the future */}
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
